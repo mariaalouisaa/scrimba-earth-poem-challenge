e7596876541e7a0364c364311cbafe75666e1088
= <maria.louisa.castro@gmail.com>
2022-04-21 16:47:45 +0100

    Rename and obj property

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ for me and for you ${this.emojis[1]}`;
   },
 };
 
+obj.color2 = "purple";
+
 document.querySelector("#poem").innerHTML = obj.sentence();
 
 // ✔️ Task: Changing only the code ABOVE (lines 1-15), complete the poem and render it on the page.
 
-// stretch goal 1️⃣: Render the most suitable emoji from the emojis array in the object somewhere in our poem.
+// ✔️ stretch goal 1️⃣: Render the most suitable emoji from the emojis array in the object somewhere in our poem.
 
 // stretch goal 2️⃣: Without changing the object declaration above, change sea color to purple!
 
